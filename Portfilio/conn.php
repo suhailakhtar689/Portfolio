65a6cd00962f3f9ea59818008ab4ff7031d733ca
Suhail Akhtar <139200803+suhailakhtar689@users.noreply.github.com>
2024-02-26 14:55:32 +0530

    Add files via upload

---
+++ Portfilio/conn.php
@@ -0,0 +1,49 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','contact_me') or die('connection failed');
+
+if(isset($_POST['send'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $number = mysqli_real_escape_string($conn, $_POST['number']);
+   $msg = mysqli_real_escape_string($conn, $_POST['message']);
+
+   $select_message = mysqli_query($conn, "SELECT * FROM `contact_form` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('query failed');
+   
+   if(mysqli_num_rows($select_message) > 0){
+      $message[] = 'message sent already!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, message) VALUES('$name', '$email', '$number', '$msg')") or die('query failed');
+      $message[] = 'message sent successfully!';
+   }
+
+}
+// $servername = "localhost";
+// $username = "username";
+// $password = "password";
+// $database = "contact_me";
+
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// if($conn->connect_error){
+//    die("connection failed" . $conn->connect_error);
+// }
+// echo "connection successfully";
+?>
+
+
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message" data-aos="zoom-out">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+
+?>
